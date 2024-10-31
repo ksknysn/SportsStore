49a728c4678ce86c045bdf24b0ebad57ee68e50a
zekaraba <arabazeka@gmail.com>
2024-10-31 19:51:01 +0300

    added a space row in pages>index.cshtml

--- SportsStore.Tests/obj/Debug/net8.0/SportsStore.Tests.AssemblyInfo.cs
+++ SportsStore.Tests/obj/Debug/net8.0/SportsStore.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09bcd23ae54fb4c4619ec1d8da3ae07da013b6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b601e54f2389a9d4ebdf2519c94b2cc728bf338")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
