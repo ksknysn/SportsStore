a609184d5a0911c4b35d7277194cf1d0b50ab83e
zekaraba <arabazeka@gmail.com>
2024-10-31 19:47:53 +0300

    gitignore changed

--- SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
+++ SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb9e105d103cd9ceb82eee70aa78b2203ed509f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09bcd23ae54fb4c4619ec1d8da3ae07da013b6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
