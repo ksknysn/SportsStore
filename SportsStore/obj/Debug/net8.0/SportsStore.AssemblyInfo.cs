06d9f6cefcfb9f7468adf24548a9d667be6d2564
zekaraba <arabazeka@gmail.com>
2024-10-31 06:40:10 +0300

    identityusers dosyası eklendi

--- SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
+++ SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb9e105d103cd9ceb82eee70aa78b2203ed509f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
